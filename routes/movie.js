70e1f4a15ac0c053743ab9e06bbdb8c50423fb71
Jose Nunez <jose.nunez@advisiondevelopment.com>
2020-11-05 15:51:28 -0600

    Refactor routes search and gender

--- routes/movie.js
+++ routes/movie.js
@@ -46,79 +46,48 @@ router.post('/add', (req, res) => {
         Image
     } = req.body;
 
-    // Data Validation
-    try {
-        var validate_name = !validator.isEmpty(Name);
-        var validate_director = !validator.isEmpty(Director);
-        var validate_year = !validator.isEmpty(Year);
-        var validate_genre = !validator.isEmpty(Gender);
-        var validate_language = !validator.isEmpty(Language);
-        var validate_favorite = !validator.isEmpty(Favorite);
-        var validate_grade = !validator.isEmpty(Community_Score);
-        var validate_image = !validator.isEmpty(Image);
-        var validate_style = !validator.isEmpty(Style);
-        var validate_popularity = !validator.isEmpty(Popularity);
+    console.log(req.body);
 
-
-    } catch (err) {
-        console.log(err);
-        return res.json({
-            'status': 404,
-            'message': 'Faltan datos por enviar',
-            'error': err
-        });
-    }
-
-    if (validate_name && validate_director && validate_year && validate_genre && validate_language && validate_favorite && validate_grade && validate_image && validate_style && validate_popularity) {
-
-        //Insert into table
-        Movie.create({
-                ID,
-                Name,
-                Director,
-                Year,
-                Gender,
-                Language,
-                Favorite,
-                Community_Score,
-                IMDB,
-                Style,
-                MetaScore,
-                Popularity,
-                Image
+    //Insert into table
+    Movie.create({
+            ID,
+            Name,
+            Director,
+            Year,
+            Gender,
+            Language,
+            Favorite,
+            Community_Score,
+            IMDB,
+            Style,
+            MetaScore,
+            Popularity,
+            Image
+        })
+        .then(movie => res.json({
+            'status': 200,
+            'message': 'Adding movies successfully',
+            'data': movie,
+        }))
+        .catch(err => {
+            console.log(err);
+            res.json({
+                'status': 404,
+                'message': 'Faltan datos por enviar',
+                'error': err,
             })
-            .then(movie => res.json({
-                'status': 200,
-                'message': 'Adding movies successfully',
-                // 'data': movies // Talvez no sea necesario, pero luego veremos, esto si se actualiza la tabla de una vez o se hace otro request
-            }))
-            .catch(err => {
-                console.log(err);
-                res.json({
-                    'status': 404,
-                    'message': 'Faltan datos por enviar',
-                    'error': err,
-                })
-            });
-    } else {
-        return res.json({
-            'status': 500,
-            'message': 'Los datos no son validos',
         });
-    }
 });
 
 // Search for name
-router.get('/search', (req, res) => {
-    let {
-        Name
-    } = req.body;
+router.get('/search/:data', (req, res) => {
+    var data_name = req.params.data;
 
     Movie.findAll({
             raw: true,
             where: {
                 Name: {
-                    [Op.like]: '%' + Name + '%'
+                    [Op.like]: '%' + data_name + '%'
                 }
             }
         })
@@ -210,16 +179,14 @@ router.put('/modify', (req, res) => {
     }
 });
 // Search by gender
-router.get('/gender', (req, res) => {
-    let {
-        Gender
-    } = req.body;
+router.get('/gender/:data', (req, res) => {
+    var data_gender = req.params.data;
 
     Movie.findAll({
             raw: true,
             where: {
                 Gender: {
-                    [Op.like]: '%' + Gender + '%'
+                    [Op.like]: '%' + data_gender + '%'
                 }
             }
         })
