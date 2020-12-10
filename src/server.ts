d19d05c1dc6691fc622bc178c8c94cc69abdf15f
Bryan Javier Badilla Amador <bbadilla17@estudiantec.cr>
2020-12-10 00:29:52 -0600

    Add new Port

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { app } from './app';
 
-app.listen(3000, () => {
-    console.log("Run on port 3000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Run");
 })
